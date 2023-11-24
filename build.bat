b88265a115d486e1b913ea200800f4be5c962b27
Alicja Niewiadomska <micek2000@gmail.com>
2023-11-24 12:55:45 +0000

    Easier running

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@ python -m venv .venv
 call .venv\Scripts\Activate.bat
 pip3 install -r requirements.txt
 rm config.json
+mkdir colldet
 touch config.json
 echo {"style": "light", "collections": []} >> config.json
 python RePrinTexApp.py
